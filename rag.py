a2ed4c5e381dafe9b46a372b62d9733c0e840912
santiagoahl <sahumadal@unal.edu.co>
2025-04-30 00:09:58 -0500

    feat: add working MVP for RAG tool

--- rag.py
+++ rag.py
@@ -1,19 +1,35 @@
 #from langchain.tools.base import BaseTool  # Superclass to build our agent
-from langchain_community.llms import HuggingFaceHub  # Use LLM models stored in HF
+#from langchain_community.llms import HuggingFaceHub  # Use LLM models stored in HF, deprecated
+from langchain_huggingface.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate  # Structure Prompts
 from langchain.docstore.document import Document  # Format documents retrieved from the datasets
-from langchain.retrievers import BM25Retriever  # Retrieve documents from dataset
-from datasets import load_dataset  # Load Guests Dataset
+from langchain_community.retrievers import BM25Retriever  # Retrieve documents from dataset
+from huggingface_hub import InferenceClient
 
-from typing import Optional, Union, Any  # Typed programming
+from datasets import load_dataset  # Load Guests Dataset
+import logging
+
+from typing import Optional, Union, Any, List  # Typed programming
 import os, getpass  # Get HF API token
 
-# TODO: Migrate _get_var to utils script
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
+handler = logging.FileHandler("app.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+for handler in logger.handlers:
+    handler.flush()
+
+
+logger.addHandler(handler)
+
+# TODO: Migrate _get_var to utils script
 # Get HF API token for model reference
 def _get_var(var) -> None:
     if os.getenv(var):
-        print("Variable already exists.")
+        logger.info(f"{var} successfully processed")
     else:
         os.environ[var] = getpass.getpass(prompt=f"Type the value of {var}: ")
 
@@ -21,37 +37,49 @@ var = "HUGGINGFACEHUB_API_TOKEN"
 _get_var(var)
 hf_token = os.getenv(var)
 
-class RAGTool():
+class RAGTool:
     def __init__(self) -> None:
         # self.index = None
         self.query: Optional[str] = None
         self.query_translated: Optional[str] = None
         self.docs: list[Document] = None
 
-        self.chat_template: Any = ChatPromptTemplate([
-            SystemMessagePromptTemplate(prompt="You are a proffesional butler, specialized to organized the most successful Galas"),
-            HumanMessagePromptTemplate(prompt="Find information about: {query}")
-        ])
+        #self.chat_template: Any = ChatPromptTemplate([
+        #    SystemMessagePromptTemplate(prompt=["You are a proffesional butler, specialized to organized the most successful Galas"]),
+        #    HumanMessagePromptTemplate(prompt=["Find information about: {query}"])
+        #])
         
-        self.llm: Any = HuggingFaceHub(
-            repo_id="Qwen/Qwen2.5-Coder-32B-Instruct",
-            huggingfacehub_api_token=hf_token,
-            parameters={
-                "temperature": 0.1,
-                "max_length": 100
-            }
+        # Deprecated and waiting for LangChain maintance
+        #self.llm: Any = HuggingFaceEndpoint(
+        #    repo_id="Qwen/Qwen2.5-Coder-32B-Instruct",
+        #    task="text-generation",
+        #    huggingfacehub_api_token=hf_token,
+        #    temperature=0.1,
+        #    max_new_tokens=100
+        #)
+        self.llm = InferenceClient(
+            model="mistralai/Mistral-7B-Instruct-v0.1", #"Qwen/Qwen2.5-Coder-32B-Instruct",
+            token=hf_token
         )
 
     def _translate_query(self) -> None:
         # Step-Back
-        prompt = f"Write step-back questions to optimize clarity: {self.query}"
-        ai_response = self.llm.invoke(prompt=prompt) # TODO: Estructurar modelo antes de pasarlo
-        self.query_translated = ai_response
+        # TODO: solve inference credits limit
+        #prompt = f"Write step-back questions to optimize clarity: {self.query}"  # Temporarily omited due to credit limits
+        #ai_response = self.llm.text_generation(
+        #    prompt=prompt,
+        #    max_new_tokens=100,
+        #    temperature=0.1
+        #) # TODO: Estructurar prompt antes de pasarlo
+        
+        #self.query_translated = ai_response 
+        self.query_translated = self.query  # TODO: delete this line
         
     def _load_dataset(self) -> None:
         # Import guest dataset
+        logger.info(self.query)
         url = "agents-course/unit3-invitees"  # TODO: Create a Basemodel subclass to save all locations
-        guest_dataset = load_dataset(path=url, name="Guest Dataset", split="train")
+        guest_dataset = load_dataset(path=url, split="train")
         
         self.docs = [
             Document(
@@ -80,7 +108,7 @@ class RAGTool():
         self.retriever = BM25Retriever.from_documents(documents=self.docs)
         
         # Similarity search
-        result = self.retriever.get_relevant_documents(self.query_translated)
+        result = self.retriever.invoke(self.query_translated)
         result = result[:3]  # Get the 3 most relevant docs
         retrieved_docs = "\n\n".join([
                     doc.page_content
@@ -97,11 +125,23 @@ class RAGTool():
         pass
     
     def __call__(self, query) -> None:
+        self.query = query
+        logger.info("ok")
         self._translate_query()
         self._load_dataset()
         retrieved_docs = self._retrieval()
+        print(
+            "\nRetrieval Successfully Completed!"
+            "\n\nSummary\n"
+            + "="*20 +
+            f"\nUser query: {self.query}\n"
+            + "="*20 +
+            f"\nRetrieved Docs:\n\n{retrieved_docs}"
+        )
+
         return retrieved_docs
 
 if __name__=="__main__":
+    logger.info("Starting RAG tool")
     retriever = RAGTool()
     retriever("Who is Ada?")
