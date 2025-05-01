a12c896ec59d55f7cd5c88127a75532b5c2f0195
santiagoahl <sahumadal@unal.edu.co>
2025-04-30 22:07:09 -0500

    feat: Built Agent, pending for debugging

---
+++ app.py
@@ -0,0 +1,33 @@
+from langgraph.graph import END, START, Node, Edge, StateGraph, State
+import os
+import rag
+import tools
+
+def app(arg1: type, arg2: type, arg3: type = default_value) -> output_type:   
+    """
+    Description.
+
+    Parameters
+    ----------
+    arg1 : type
+        Description
+    arg2 : type
+        Description
+    arg3 : type
+        Description
+    
+    Returns:
+        type:
+    
+    Example:
+        >>> ('arg1', 'arg2')
+        'output'
+    """
+    return None
+
+
+def run():
+    pass
+
+if __name__=="__main__":
+    run()
